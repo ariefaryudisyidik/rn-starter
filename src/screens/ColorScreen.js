4b3759352da7cb3c928d944cd755f9b7daa1fb1e
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2022-08-08 14:50:02 +0700

    Color

---
+++ src/screens/ColorScreen.js
@@ -0,0 +1,39 @@
+import { Button, FlatList, StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+
+const ColorScreen = () => {
+  const [colors, setColors] = useState([]);
+  console.log(colors);
+
+  return (
+    <View>
+      <Button
+        title="Add a Color"
+        onPress={() => {
+          setColors([...colors, randomRgb()]);
+        }}
+      />
+      <FlatList
+        keyExtractor={(item) => item}
+        data={colors}
+        renderItem={({ item }) => {
+          return (
+            <View style={{ width: 100, height: 100, backgroundColor: item }} />
+          );
+        }}
+      />
+    </View>
+  );
+};
+
+const randomRgb = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  return `rgb(${red}, ${green}, ${blue})`;
+};
+
+export default ColorScreen;
+
+const styles = StyleSheet.create({});
